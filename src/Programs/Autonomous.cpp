f027db386951bb866b3a4b2aa8583b0303ee6801
Ansel Chang <ansel.chang@yahoo.com>
2022-12-13 04:11:13 -0500

    incremental optimizations to auton

--- src/Programs/Autonomous.cpp
+++ src/Programs/Autonomous.cpp
@@ -13,8 +13,8 @@
 #define GFU_DIST(maxSpeed) SingleBoundedPID({0.1, 0, 0, 0.1, maxSpeed})
 #define GFU_DIST_PRECISE(maxSpeed) DoubleBoundedPID({0.1, 0, 0, 0.1, maxSpeed}, 0.2, 3)
 #define GFU_TURN SimplePID({1, 0, 0.1, 0.0, 1})
-#define GTU_TURN DoubleBoundedPID({1, 0, 0.1}, getRadians(1.5), 3)
-#define GTU_TURN_PRECISE DoubleBoundedPID({0.8, 0, 0.1}, getRadians(0.75), 5)
+#define GTU_TURN DoubleBoundedPID({1.25, 0, 0.1, 0.12, 1}, getRadians(1.5), 3)
+#define GTU_TURN_PRECISE DoubleBoundedPID({1.25, 0, 0.1, 0.12, 1}, getRadians(0.75), 3)
 #define GCU_CURVE SimplePID({1.7, 0, 0})
 
 
@@ -39,10 +39,10 @@ void shoot(Robot& robot) {
     // wait for spinup
     if (!robot.flywheel->atTargetVelocity()) {
 
-        constexpr int32_t TIMEOUT_MS = 5000; // maximum time to wait for spinup to proper velocity
+        constexpr uint32_t TIMEOUT_MS = 5000; // maximum time to wait for spinup to proper velocity
 
         setEffort(*robot.intake, 0);
-        int32_t start = pros::millis();
+        uint32_t start = pros::millis();
         while (!robot.flywheel->atTargetVelocity() && pros::millis() - start < TIMEOUT_MS) {
             pros::delay(10);
         }
@@ -70,14 +70,6 @@ void shoot(Robot& robot) {
     pros::Task([&] {delayResetIndexer(robot); });
 }
 
-void testCurve(Robot& robot) {
-    robot.flywheel->setVelocity(0);
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, getRadians(0), getRadians(90), 24);
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, getRadians(90), getRadians(0), 24);
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, getRadians(0), getRadians(90), -24);
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, getRadians(90), getRadians(0), -24);
-}
-
 void matchAutonIMUOnly(Robot& robot) {
 
 	
@@ -98,18 +90,21 @@ void matchAutonIMUOnly(Robot& robot) {
     robot.flywheel->setVelocity(3350); // next shot is a bit further away
 
     // Get to roller
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, startTheta, getRadians(67), -15);
+    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, startTheta, getRadians(75), -12);
     goTurnU(robot, GTU_TURN, 0);
-    goForwardTimedU(robot, GFU_TURN, 1, -0.5, 0);
+    goForwardTimedU(robot, GFU_TURN, 1, -0.5, 0); // back up till pressed with roller
+
+    // Do roller [TODO]
 
     // Collect disc at (1,1)
     setEffort(*robot.intake, 1);
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, 0, getRadians(-55), 14);
+    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, 0, getRadians(-30), 16);
     pros::delay(500); // wait to pickup disc
 
     // Collect disc at (0.5, 0.5)
-    goTurnU(robot, GTU_TURN, getRadians(-115));
-    goForwardU(robot, GFU_DIST_PRECISE(0.8), GFU_TURN, 12, getRadians(-115));
+    goForwardU(robot, GFU_DIST(0.8), GFU_TURN, -8, getRadians(-30));
+    goTurnU(robot, GTU_TURN, getRadians(-90));
+    goForwardU(robot, GFU_DIST_PRECISE(0.8), GFU_TURN, 10, getRadians(-90));
     pros::delay(500); // wait to pickup disc
 
     // Shoot two discs
@@ -124,14 +119,6 @@ void matchAutonIMUOnly(Robot& robot) {
     // Shoot preloads
     goTurnU(robot, GTU_TURN_PRECISE, getRadians(-20));
     shoot(robot);
-
-
-
-
-    // Do roller [TODO]
-
-
-
     
 
 	pros::lcd::print(0, "done");
