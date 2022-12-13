be8a08a982a9911475bd304b90394ae4d3719db1
Ansel Chang <ansel.chang@yahoo.com>
2022-12-13 01:59:46 -0500

    improvements to autn

--- src/Programs/Autonomous.cpp
+++ src/Programs/Autonomous.cpp
@@ -13,6 +13,7 @@
 #define GFU_DIST_PRECISE(maxSpeed) DoubleBoundedPID({0.1, 0, 0, 0.1, maxSpeed}, 0.2, 3)
 #define GFU_TURN SimplePID({1, 0, 0.1, 0.0, 1})
 #define GTU_TURN DoubleBoundedPID({1, 0, 0.1}, getRadians(1.5), 3)
+#define GTU_TURN_PRECISE DoubleBoundedPID({0.8, 0, 0.1}, getRadians(0.75), 5)
 #define GCU_CURVE SimplePID({1.7, 0, 0})
 
 
@@ -23,6 +24,17 @@ void startIntake(Robot& robot) {
 
 // shoot a 3-burst round. First two rounds are short burst (110ms with 220ms break), third is longer (300ms)
 void shoot(Robot& robot) {
+
+    // cock gun
+    setEffort(*robot.intake, 1);
+    robot.indexer->set_value(true);
+    pros::delay(500);
+
+    // wait for spinup
+    if (!robot.flywheel->atTargetVelocity()) {
+        setEffort(*robot.intake, 0);
+        while (!robot.flywheel->atTargetVelocity()) pros::delay(10);
+    }
 
     setEffort(*robot.intake, -1);
     pros::delay(110);
@@ -55,7 +67,7 @@ void matchAutonIMUOnly(Robot& robot) {
     robot.localizer->setPosition(0, 0);
 	robot.flywheel->setVelocity(3250);
 
-    double startTheta = getRadians(346);
+    double startTheta = getRadians(342);
 
     // initial goal rush
     pros::Task([&] { startIntake(robot); }); // start intake 300ms after goForwardU
@@ -63,30 +75,27 @@ void matchAutonIMUOnly(Robot& robot) {
 
     pros::delay(1000);
 
-    // cock the gun
-    robot.indexer->set_value(true);
-    pros::delay(500);
-
-    // wait for spinup
-    setEffort(*robot.intake, 0);
-    while (!robot.flywheel->atTargetVelocity()) pros::delay(10);
-
     // shoot
     shoot(robot);
+    robot.flywheel->setVelocity(3350); // next shot is a bit further away
 
     // Get to roller
     goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, startTheta, getRadians(67), -15);
     goTurnU(robot, GTU_TURN, 0);
     goForwardTimedU(robot, GFU_TURN, 1, -0.5, 0);
 
-    // Collect two discs at corner
+    // Collect disc at (1,1)
     setEffort(*robot.intake, 1);
-    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, 0, getRadians(-45), 10);
-    goTurnU(robot, GTU_TURN, getRadians(-90));
-    goForwardU(robot, GFU_DIST_PRECISE(0.8), GFU_TURN, 8);
+    goCurveU(robot, GFU_DIST_PRECISE(0.5), GCU_CURVE, 0, getRadians(-55), 14);
+    pros::delay(500);
+
+    // Collect disc at (0.5, 0.5)
+    goTurnU(robot, GTU_TURN, getRadians(-115));
+    goForwardU(robot, GFU_DIST_PRECISE(0.8), GFU_TURN, 16, getRadians(-115));
+    pros::delay(500);
 
     // Shoot two discs
-    goTurnU(robot, GTU_TURN, 0);
+    goTurnU(robot, GTU_TURN_PRECISE, 0);
     shoot(robot);
 
     // Collect preloads
@@ -95,7 +104,7 @@ void matchAutonIMUOnly(Robot& robot) {
     goForwardU(robot, GFU_DIST_PRECISE(0.8), GFU_TURN, 24, getRadians(80));
 
     // Shoot preloads
-    goTurnU(robot, GTU_TURN, getRadians(-15));
+    goTurnU(robot, GTU_TURN_PRECISE, getRadians(-20));
     shoot(robot);
 
 
