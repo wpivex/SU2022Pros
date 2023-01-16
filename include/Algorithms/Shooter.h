c5b111ad8978d014241699c95176b06586a93a57
Matthew Papesh <55628349+MattPapesh@users.noreply.github.com>
2023-01-16 15:28:14 -0500

    push shooter algo

--- include/Algorithms/Shooter.h
+++ include/Algorithms/Shooter.h
@@ -12,27 +12,34 @@ private:
     const double AFTER_INTAKE_SPEED = -1;
 
     int state = 0;
+    int discNum = 0;
     /*
-    State = 0: flywheel has not yet shot first disk
-    State = 1: flywheel has shot first disk
+    State = 0: flywheel has not yet shot
+    State = 1: flywheel has shot and speed has decreased
     */
 
 public:
 
     void reset() {
         state = 0;
+        discNum = 0;
     }
 
     double tickIntakeShootingSpeed(Robot& robot) {
 
         double diff = robot.flywheel->getTargetVelocity() - robot.flywheel->getCurrentVelocity();
 
-        if (diff > 75) state = 1; // state == 1 means that the flyweel already shot the first disk
+        if (state == 0 && diff > 75) { // flywheel slowed down. Means we just finished shooting a disc
+            discNum++;
+            state = 1;
+        } else if (state == 1 && diff < 20) { // we've reached back to full velocity, ready for the next shot
+            state = 0;
+        }
 
-        if (diff > 50) { // if below target velocity by this number, stop flywheel
+        if (state == 1) { // if velocity is low after shooting disc, stop flywheel
             return 0;
         } else {
-            return (state == 1) ? AFTER_INTAKE_SPEED : FIRST_INTAKE_SPEED;
+            return (discNum == 0) ? FIRST_INTAKE_SPEED : AFTER_INTAKE_SPEED;
         }
     }
 };
