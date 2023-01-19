92001c4325616e3d8301d9bb08d8b8d226ef9081
Ansel Chang <ansel.chang@yahoo.com>
2023-01-19 05:25:16 -0500

    robot18 ports. no endgame yet

--- src/Subsystems/RobotBuilder.cpp
+++ src/Subsystems/RobotBuilder.cpp
@@ -55,21 +55,22 @@ Robot getRobot15() {
 
 }
 
+
 Robot getRobot18() {
 
     Robot robot;
 
     robot.drive.reset(new Drive(
-        {1, 3, -4,-5}, // left motor ports
-        {-16, -17, 18, 19}, // right motor ports
+        {-3, 4, -5, 6}, // left motor ports
+        {-9, 16, 18, -19}, // right motor ports
         pros::E_MOTOR_GEAR_600, // internal gear ratio
         3.0/4.0, // external gear ratio
         2.74, // wheel diameter in inches
-        15.2 // track width in inches
+        14.25//15.2 // track width in inches
     ));
 
     robot.localizer.reset(new Odometry(
-        2, // imu port
+        13, // imu port
         {'E','F'}, // left encoder port
         {'C', 'D'}, // right encoder port
         {'A', 'B'}, // back encoder port
@@ -78,22 +79,22 @@ Robot getRobot18() {
     ));
 
     robot.flywheel.reset(new TBHFlywheel(
-        {17, 18}, // ports
+        {-1, 2}, // ports
         0.0002, // tbh constant
         { // volt to rpm data
-            {1570, 5},
-            {1970, 6},
-            {2315, 7},
-            {2650, 8},
-            {3050, 9},
-            {3400, 10},
-            {3740, 11},
-            {4130, 12}
+            {1615, 5},
+            {1966, 6},
+            {2306, 7},
+            {2646, 8},
+            {3054, 9},
+            {3416, 10},
+            {3751, 11},
+            {4141, 12}
         },
         0 // start speed
     ));
 
-    robot.intake.reset(new pros::MotorGroup({9, 10}));
+    robot.intake.reset(new pros::MotorGroup({11, -12}));
     robot.intake->set_brake_modes(pros::E_MOTOR_BRAKE_BRAKE);
 
     robot.indexer.reset(new pros::ADIDigitalOut('G'));
